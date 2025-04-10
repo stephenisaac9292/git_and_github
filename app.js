108715862f3a5601f96be0cda7035e9ae0a75ff0
Stephen Isaac <stephenisaac9292@gmail.com>
2025-04-10 14:58:55 +0100

    third

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("sfjksjf");
+let age = prompt("Enter your age: ");
+
+if (age>18){
+    console.log(`You are ${age}, years old, go ahead and enter the club` );
+    }
+else if (age===18){
+    console.log(`Welcome to adulthood; get in`);
+    }
+else {
+    console.log(`You are ${age}, years old, Please go home`);
+    }
